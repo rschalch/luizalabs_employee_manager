1ae755917f0db993f2b14e791b72aba0452aab53
rbs <ricardo.schalch@gmail.com>
2016-11-23 13:49:01 -0200

    change default url to django admin system updated instructions.txt

--- luizalabs_employee_manager/luizalabs_employee_manager/urls.py
+++ luizalabs_employee_manager/luizalabs_employee_manager/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from employees import views
 
 urlpatterns = [
+    url(r'', admin.site.urls),
     url(r'^admin/', admin.site.urls),
-    url(r'^employee/$', views.EmployeeViewSet.as_view({'get': 'list'}), name='employee-list'),
+    url(r'^employee/$', views.EmployeeViewSet.as_view({'get': 'list'}),
+        name='employee-list'),
 ]
